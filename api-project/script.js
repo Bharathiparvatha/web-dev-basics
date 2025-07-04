e67c907a871586cb2ae3e96b6fb544fd024239e9
Bharathiparvatha <bharathiparvatha@gmail.com>
2025-07-04 15:10:32 +0530

    Delete api-project directory

--- api-project/script.js
+++
@@ -1,5 +0,0 @@
-async function getJoke() {
-  const res = await fetch('https://v2.jokeapi.dev/joke/Any?type=single');
-  const data = await res.json();
-  document.getElementById("joke").innerText = data.joke || "Failed to load joke.";
-}
